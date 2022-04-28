b0faf6dda2b34c06fbd6729885b298916c1cb9ee
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-04-28 12:56:08 -0500

    Updated server file

--- Module-3-REST/src/server.ts
+++ Module-3-REST/src/server.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import express, {Request,Response} from 'express'
-import cors from 'cors'
+import express, {NextFunction, Request,Response} from 'express'
+import cors, { CorsOptions } from 'cors'
 import { router } from './router'
+import { HttpError } from 'http-errors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -10,13 +11,43 @@ const ENVIROMENT = process.env.NODE_ENV || 'development'
 app.set('port',process.env.PORT || 3000) 
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+const whiteList = ['http://localhost:3000']
+const corsOptionsDelegate = function handler(
+  req: Request,
+  callback: (err: Error | null, options?: CorsOptions) => void,
+) {
+  const corsOptions: { origin: boolean } = { origin: false }
+
+  if (whiteList.indexOf(req.header('Origin') ?? '') !== -1) {
+    corsOptions.origin = true
+  }
+
+  callback(null, corsOptions)
+}
+
+function errorHandler(
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  if (ENVIROMENT !== 'development') {
+    console.error(err.message)
+    console.error(err.stack || '')
+  }
+
+  res.status(err.status ?? 500)
+  res.json(err)
+}
+
+app.use(cors(corsOptionsDelegate))
 app.get('/api/v1/status', (req: Request, res: Response) => {
     res.json({ time: new Date() })
   })
 app.use('/', router(app))
+app.use(errorHandler)
 app.listen(PORT,async () => {
-    // eslint-disable-next-line no-console
     console.log(`Server listening on port %d, env: %s`, PORT, ENVIROMENT)
   })
