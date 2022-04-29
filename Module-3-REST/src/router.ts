c25df67cde0fa1cd0b4e6056fa81c9d6f6b6196f
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-04-29 18:02:25 -0500

    Added Likes router and routes in comments and post

--- Module-3-REST/src/router.ts
+++ Module-3-REST/src/router.ts
@@ -6,6 +6,7 @@ const expressRouter = express.Router()
 export function router(app:Router):Router {
     // app.use('/api/v1/auth', authRoutes())
     app.use('/api/v1/accounts', usersRoutes())
-    app.use('/api/v1/posts',[postsRoutes(),commentsRoutes()])
+    app.use('/api/v1/posts',postsRoutes())
+    app.use('/api/v1/posts/:idPost/comments', commentsRoutes())
     return expressRouter
 }
--- Module-3-REST/src/routes/posts.route.ts
+++ Module-3-REST/src/routes/posts.route.ts
@@ -7,6 +7,12 @@ import {
     findOne,
     create as createPost
 } from '../controllers/posts.controller'
+
+import {
+    findLikesPost,
+    updateLikePost,
+    deleteLike
+} from '../controllers/likes.controller'
 const router = express.Router()
 export function postsRoutes(): Router {
     router
@@ -16,10 +22,18 @@ export function postsRoutes(): Router {
         .post(asyncHandler(createPost))
     
     router
-        .route('/:id')
+        .route('/:idPost')
         // .all(passport.authenticate('jwt', {session:false}))
         .get(asyncHandler(findOne))
         .patch(asyncHandler(update))
-        .delete(asyncHandler(deletePost))   
+        .delete(asyncHandler(deletePost))  
+    router
+        .route('/:idPost/likes')
+        // .all(passport.authenticate('jwt', {session:false}))
+        .get(asyncHandler(findLikesPost))
+        .patch(asyncHandler(updateLikePost))
+    router
+        .route('/:idPost/likes/:idLike')
+        .delete(asyncHandler(deleteLike))   
     return router
 }
