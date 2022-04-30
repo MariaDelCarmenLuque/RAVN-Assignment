40795fa595942c1a932b8bc786726dcfb74d65bb
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-04-30 01:14:31 -0500

    Added Auth routerÂs in routes

---
+++ Module-3-REST/src/routes/auth.route.ts
@@ -0,0 +1,20 @@
+import express, { Router } from 'express'
+import asyncHandler from 'express-async-handler'
+import { login } from '../controllers/auth.controller'
+import { register } from '../controllers/users.controller'
+
+
+const router = express.Router()
+
+export function authRoutes(): Router {
+  router
+  .route('/login')
+  .post(asyncHandler(login))
+//   router.route('/logout').post(asyncHandler(logout))
+  router
+  .route('/register')
+  .post(asyncHandler(register))
+//   router.route('/confirmUser').post(asyncHandler(confirmUser))
+
+  return router
+}
